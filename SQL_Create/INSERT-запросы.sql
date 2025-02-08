2d032cda48c3c0048a078dc4c322e0df2fcd0534
Evgenii <Ponomarev_ep@bk.ru>
2025-02-08 14:39:54 +0500

    Correction_comments

--- SQL_Create/INSERT-запросы.sql
+++ SQL_Create/INSERT-запросы.sql
@@ -1,56 +1,124 @@
-INSERT INTO performers (performerid,  name)
-VALUES (1, 'Игорь Николаев');
-
-CREATE SEQUENCE performers_performerid_seq;
-
-ALTER TABLE performers ALTER COLUMN performerid SET DEFAULT nextval('performers_performerid_seq');
+INSERT INTO performers (name)
+VALUES ('Игорь Николаев')
 
 INSERT INTO performers (name)
-VALUES ('Ария');
+VALUES ('Ария')
 
-CREATE SEQUENCE musical_genres_genreid_seq;
+INSERT INTO performers (name)
+VALUES ('Король и шут')
 
-ALTER TABLE musical_genres ALTER COLUMN genreid SET DEFAULT nextval('musical_genres_genreid_seq');
+INSERT INTO performers (name)
+VALUES ('Руки вверх')
+
+
+
 
 insert into musical_genres (name)
-values ('R&B/Soul')
+values ('Рок')
 
-update musical_genres 
-set name = 'Rock (рок)'
-where genreid = 1
+insert into musical_genres (name)
+values ('ПОП')
 
-CREATE SEQUENCE tracks_trackid_seq;
+insert into musical_genres (name)
+values ('Классика')
 
-ALTER TABLE tracks ALTER COLUMN trackid SET DEFAULT nextval('tracks_trackid_seq');
+
 
 insert  into albums (name, year)
 values ('Как в старой сказке', '2001-11-12')
 
+insert  into albums (name, year)
+values ('Открой мне дверь!', '2012-11-12')
+
+insert  into albums (name, year)
+values ('Гость из царства теней', '2020-11-12')
+
+insert  into albums (name, year)
+values ('Проклятье морей', '2018-11-12')
+
+
 
 insert into tracks (name, duration, album_id)
-values ('Антихрист', '05:02', 1)
+values ('Антихрист', '03:25', 3)
 
 insert into tracks (name, duration, album_id)
-values ('Гимн шута', '05:01', 4)
+values ('Гимн шута', '05:01', 1)
+
+insert into tracks (name, duration, album_id)
+values ('Крошка мой', '04:12', 2)
+
+insert into tracks (name, duration, album_id)
+values ('18 мне уже', '04:07', 2)
+
+insert into tracks (name, duration, album_id)
+values ('Варяг', '04:53', 4)
+
+insert into tracks (name, duration, album_id)
+values ('Эра Люцифера', '05:46', 4)
 
 
-CREATE SEQUENCE collection_collectionid_seq;
-
-ALTER TABLE collection ALTER COLUMN collectionid SET DEFAULT nextval('collection_collectionid_seq');
 
 insert into collection (name, year)
-values ('My collection 5', '2021-11-12')
+values ('My collection 1', '2017-11-12')
+
+insert into collection (name, year)
+values ('My collection 2', '2018-11-12')
+
+insert into collection (name, year)
+values ('My collection 3', '2019-11-12')
+
+insert into collection (name, year)
+values ('My collection 4', '2020-11-12')
+
+
 
 insert into perfomers_ganres (genre_id, pefomer_id)
-values(1, 6)
+values(2, 1)
+
+insert into perfomers_ganres (genre_id, pefomer_id)
+values(1, 2)
+
+insert into perfomers_ganres (genre_id, pefomer_id)
+values(1, 3)
+
+insert into perfomers_ganres (genre_id, pefomer_id)
+values(2, 4)
+
+
+
+
+
+insert into participation_in_albums (performer_id, album_id)
+values(3, 1)
+
+insert into participation_in_albums (performer_id, album_id)
+values(4, 2)
+
+insert into participation_in_albums (performer_id, album_id)
+values(2, 3)
 
 insert into participation_in_albums (performer_id, album_id)
 values(2, 4)
 
+
+
+
+
 insert into tracks_in_collection (track_id, collection_id)
-values(7, 4)
+values(1, 1)
 
-DELETE FROM tracks_in_collection
-WHERE track_id = 7 AND collection_id = 4 and id = 8;
+insert into tracks_in_collection (track_id, collection_id)
+values(2, 2)
 
+insert into tracks_in_collection (track_id, collection_id)
+values(3, 2)
+
+insert into tracks_in_collection (track_id, collection_id)
+values(4, 3)
+
+insert into tracks_in_collection (track_id, collection_id)
+values(5, 4)
+
+insert into tracks_in_collection (track_id, collection_id)
+values(6, 4)
 
--- SQL_Create/SELECT-запросы.sql
+++ SQL_Create/SELECT-запросы.sql
@@ -5,11 +5,11 @@ LIMIT 1
 
 SELECT name, duration
 FROM tracks
-WHERE duration::interval > '03:30:00'
+WHERE duration::interval >= '03:30'
 
 select name
 from collection
-where year > '2018-01-01' and year < '2020-12-31'
+where year >= '2018-01-01' and year <= '2020-12-31'
 
 select *
 from performers
@@ -17,7 +17,7 @@ where name ~* '^[а-яА-Я]+$'
 
 select name
 from tracks
-where name ilike '%мой%' or name ilike '%my%'
+where name ~* '\y(мой|my)\y'
 
 select mg.name as Genre, COUNT(pg.pefomer_id) as Performer
 from perfomers_ganres pg
@@ -30,25 +30,23 @@ from tracks t
 join albums a on t.album_id = a.albumid 
 where a.year between '2019-01-01' AND '2020-12-31'
 
-
-
 select a.name as Альбом, AVG(t.duration) as Средняя_продолжительность
 from tracks t
 join albums a on t.album_ID = a.albumID
 group by a.name
 order by a.name
 
-
-select distinct p.name
+select p.name
 from performers p
-left join participation_in_albums pa on p.performerid = pa.performer_id 
-left join albums a ON pa.album_id = a.albumid
-where (a.year > '2020-12-31' and a.year < '2020-01-01') or a.year is null 
-
+where p.performerid not in (
+    select pa.performer_id
+    from participation_in_albums pa
+    join albums a on pa.album_id = a.albumid
+    where a.year >= '2020-01-01' and a.year <= '2020-12-31')
 
 select distinct c.name
 from collection c
 join tracks_in_collection tic on c.collectionid = tic.collection_id
 join tracks t on tic.track_id = t.trackid
 join participation_in_albums pia on t.album_id = pia.album_id
-where pia.performer_id = 6
+where pia.performer_id = 2
